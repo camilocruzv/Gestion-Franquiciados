5b1ab1914ed6c76619b068e8820a2acf6e019b66
marianar97 <marianar97@LAPTOP-J03EB5V8.localdomain>
2019-06-04 18:03:23 -0500

    fixing errors

--- app/index.js
+++ app/index.js
@@ -12,15 +12,6 @@ const flash = require('connect-flash');
 var expressValidator = require('express-validator');
 server.use(expressValidator())
 
-
-//let dbConn = 'mongodb://user1:123456a@ds025232.mlab.com:25232/eacidb';
-//let dbConn = 'mongodb://localhost/CRM';
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then( (req, res) => {
-  console.log("Conectado a la base de datos exitosamente");
-}).catch( err => {
-  console.log("Error al conectarse a la base de datos: ", err);
-});
-
 // Middleware
 const bodyParser = require('body-parser');
 server.use(express.static(path.join(__dirname, 'public')));
@@ -46,22 +37,22 @@ server.use(passport.session());
 server.use(routes);
 
 
-
+/*
 server.set('port', process.env.PORT || 5000);
 
 server.listen(server.get('port'), () => {
   console.log("server on port ${server.get('port')}");
 });
+*/
 
 
-/*
+
 const PORT = 8000;
 const HOST = '0.0.0.0'; 
-
 server.listen(PORT, HOST, function(req, res){
     console.log('\nApp web corriendo en http://localhost:'+PORT+'\n');
   });
-  */
+
 
 
   server.get('/', (req, res) => {
@@ -96,7 +87,3 @@ server.use(function(req, res, next) {
   res.locals.error = req.flash('error');
   next();
 });
-
-
-
-
